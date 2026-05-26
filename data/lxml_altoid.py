5a48890cafc24cc309d3d383d2cc3c6d1d7c8c71
FourbeFlo <floriane.goyus@gmail.com>
2026-05-26 14:44:07 +0200

    add test data

--- data/lxml_altoid.py
+++ data/lxml_altoid.py
@@ -1,7 +1,7 @@
 import glob
 from lxml import etree
 
-folder_path = "Bullinger_1-Tim_C1/*.xml"
+folder_path = "Bugenhagen_rm_fin_test/*.xml"
 
 ALTO_NS = "http://www.loc.gov/standards/alto/ns-v4#"
 NSMAP = {"alto": ALTO_NS}
